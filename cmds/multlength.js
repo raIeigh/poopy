ed646ca1ea5d5cf7a6832d324eb0c25af3a5fb88
raIeigh <58990470+raIeigh@users.noreply.github.com>
2022-04-30 15:09:42 +0100

    improved and renamed 2xlength cmd to multlength

--- cmds/multlength.js
+++ cmds/multlength.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: ['2xlength', '2xduration', 'doublelength', 'doubleduration'],
+    name: ['multlength', 'multduration'],
     execute: async function (msg, args) {
         let poopy = this
 
@@ -10,6 +10,7 @@ module.exports = {
             return;
         };
         var currenturl = poopy.data[poopy.config.mongodatabase]['guild-data'][msg.guild.id]['channels'][msg.channel.id]['lastUrl'] || args[1]
+        var speed = isNaN(Number(args[1])) ? 2 : Number(args[1]) <= 1 ? 1 : Number(args[1]) >= 6 ? 6 : Number(args[1]) || 2
         var fileinfo = await poopy.functions.validateFile(currenturl).catch(error => {
             msg.channel.send(error)
             msg.channel.sendTyping().catch(() => { })
@@ -27,7 +28,7 @@ module.exports = {
             var videohex = poopy.modules.fs.readFileSync(`${filepath}/${filename}`)
             var mvhdindex = videohex.indexOf('mvhd')
             var subarray1 = videohex.subarray(0, mvhdindex + 20)
-            var doublehex = (Number('0x' + videohex.toString('hex').substring((mvhdindex + 20) * 2, (mvhdindex + 24) * 2)) * 2).toString(16).padStart(8, '0')
+            var doublehex = (Number('0x' + videohex.toString('hex').substring((mvhdindex + 20) * speed, (mvhdindex + 24) * speed)) * speed).toString(16).padStart(8, '0')
             var doublelength = Buffer.from(doublehex.substring(doublehex.length - 8, doublehex.length), 'hex')
             var subarray2 = videohex.subarray(subarray1.length + doublelength.length, videohex.length)
             var newvideohex = Buffer.concat([subarray1, doublelength, subarray2])
@@ -45,8 +46,8 @@ module.exports = {
         }
     },
     help: {
-        name: '2xlength/2xduration/doublelength/doubleduration <video>',
-        value: "Manipulates the video's Hex Code to double its duration."
+        name: 'multlength/multduration [multiplier (from 1 to 6)] <video>',
+        value: "Manipulates the video's Hex Code to multiply its duration."
     },
     cooldown: 2500,
     type: 'Hex Manipulation'
