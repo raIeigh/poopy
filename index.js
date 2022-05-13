79eda0275bdaa720688d45019372640e7e5a35fd
raleigh <rbnpt@hotmail.com>
2022-05-13 10:32:15 +0100

    api/command endpoint test

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ async function main() {
     const express = require('express')
     const cors = require('cors')
     const bp = require('body-parser')
+    const { EventEmitter } = require('events')
     const fs = require('fs-extra')
     const axios = require('axios')
     const md5 = require('md5')
@@ -12,13 +13,17 @@ async function main() {
 
     const PORT = process.env.PORT || 8080
     const app = express()
+    const eventEmitter = new EventEmitter()
 
     app.use(cors())
-
     app.use(bp.json())
     app.use(bp.urlencoded({ extended: true }))
 
-    /*app.use(function (req, res, next) {
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms))
+    }
+
+    app.use(function (req, res, next) {
         const isNotSecure = (!req.get('x-forwarded-port') && req.protocol !== 'https') ||
             parseInt(req.get('x-forwarded-port'), 10) !== 443 &&
             (parseInt(req.get('x-forwarded-port'), 10) === parseInt(req.get('x-forwarded-port'), 10))
@@ -28,23 +33,34 @@ async function main() {
         }
 
         next()
-    })*/
+    })
 
-    app.get('/api/poopystarted', async function (req, res) {
-        res.type('json').send({
-            started: poopyStarted
+    app.get('/api/onpoopystart', async function (req, res) {
+        res.set({
+            'Cache-Control': 'no-cache',
+            'Content-Type': 'text/event-stream',
+            'Connection': 'keep-alive'
         })
+        res.flushHeaders()
+
+        eventEmitter.once('poopystart', () => res.write(`data: true\n\n`))
+
+        res.write('retry: 10000\n\n')
+    })
+
+    app.get('/api/text', async function (req, res) {
+        for (var i = 0; i < 10; i++) {
+            await sleep(1000)
+            res.write('fazballs')
+        }
     })
 
     app.post('/api/command', async function (req, res) {
         if (poopyStarted) {
             let sent = false
 
-            console.log(req.body)
-
             if (!req.body || req.body.command != undefined) {
-                res.status(400).type('text').send('You need a command name.')
-                return
+                return res.status(400).type('text').send('You need a command name.')
             }
 
             let msg = {
@@ -93,18 +109,16 @@ async function main() {
 
             msg.guild = {
                 name: 'Guild',
-                members: {
-                    fetch: async () => msg.member,
-                    cache: new Map([[md5(req.ip), msg.member]])
+                fetchAuditLogs: async () => {
+                    return {
+                        entries: new Map()
+                    }
                 },
                 emojis: {
                     cache: new Map()
                 },
-                channels: {
-                    cache: new Map()
-                },
                 ownerID: md5(req.ip),
-                id: 'apiguild'
+                id: md5(req.ip).reverse()
             }
 
             msg.channel = {
@@ -153,7 +167,7 @@ async function main() {
                                 if (embed.author && embed.author.name) contents.push(embed.author.name)
                                 if (embed.title) contents.push(embed.title)
                                 if (embed.description) contents.push(embed.description)
-                                if (embed.fields) contents.push(embed.fields.map(field => `${field.name ?? ''}\n${field.value ?? ''}`).join('\n'))
+                                if (embed.fields && embed.fields.length > 0) contents.push(embed.fields.map(field => `${field.name ?? ''}\n${field.value ?? ''}`).join('\n'))
                                 if (embed.footer && embed.footer.text) contents.push(embed.footer.text)
                             })
 
@@ -169,11 +183,6 @@ async function main() {
 
                 owner: mainPoopy.bot.user,
 
-                messages: {
-                    fetch: async () => msg,
-                    cache: new Map([['apimessage', msg]])
-                },
-
                 type: 'GUILD_TEXT',
 
                 id: 'apichannel'
@@ -182,6 +191,7 @@ async function main() {
             msg.author = {
                 send: async () => msg,
                 displayAvatarURL: () => mainPoopy.bot.user.displayAvatarURL({ dynamic: true, size: 1024, format: 'png' }),
+                createDM: async () => msg.channel,
                 username: 'User',
                 dmChannel: msg.channel,
                 id: md5(req.ip)
@@ -192,20 +202,53 @@ async function main() {
                 user: msg.author,
                 nickname: 'Member',
                 guild: msg.guild,
+                roles: {
+                    cache: new Map([['apirole', { name: 'owner', id: 'apirole' }]])
+                },
                 permissions: {
                     has: () => true
                 },
                 id: md5(req.ip)
             }
 
+            msg.guild.members = {
+                fetch: async () => msg.member,
+                cache: new Map([[md5(req.ip), msg.member]])
+            }
+
+            msg.guild.channels = {
+                cache: new Map([['apichannel', msg.channel]])
+            }
+
+            msg.channel.messages = {
+                fetch: async () => msg,
+                cache: new Map([['apimessage', msg]])
+            }
+
+            if (poopy.tempdata[msg.guild.id][msg.channel.id]['shut']) {
+                return res.status(400).type('text').send('shut')
+            }
+
+            if (poopy.data['guild-data'][msg.guild.id]['members'][msg.author.id]['coolDown']) {
+                if ((poopy.data['guild-data'][msg.guild.id]['members'][msg.author.id]['coolDown'] - Date.now()) > 0) {
+                    return res.status(400).type('text').send(`Calm down! Wait more ${(poopy.data['guild-data'][msg.guild.id]['members'][msg.author.id]['coolDown'] - Date.now()) / 1000} seconds.`)
+                } else {
+                    poopy.data['guild-data'][msg.guild.id]['members'][msg.author.id]['coolDown'] = false
+                }
+            }
+
+            if (poopy.config.shit.find(id => id === msg.author.id)) {
+                return res.status(400).type('text').send('shit')
+            }
+
             const commandname = req.body.command.toLowerCase()
 
-            const command = mainPoopy.functions.findCommand(req.body.command.toLowerCase())
+            const command = mainPoopy.functions.findCommand(commandname)
             const localCommand = mainPoopy.data['guild-data'][msg.guild.id]['localcmds'].find(cmd => cmd.name === commandname)
 
             if (command || localCommand) {
                 if (mainPoopy.data['guild-data'][msg.guild.id]['disabled'].find(cmd => cmd.find(n => n === commandname))) {
-                    res.status(400).type('text').send('This command was disabled by someone.')
+                    return res.status(400).type('text').send('This command is disabled here.')
                 }
 
                 await mainPoopy.functions.gatherData(msg).catch(() => { })
@@ -219,11 +262,38 @@ async function main() {
 
                 const args = msg.content.split(' ')
 
-                await command.execute.call(mainPoopy, msg, args).catch((e) => {
-                    if (!sent) res.type('text').send(e.stack)
-                })
-            } else if (!sent) {
-                res.status(400).type('text').send('Command not found.')
+                if (command) {
+                    if (command.cooldown) {
+                        poopy.data['guild-data'][msg.guild.id]['members'][msg.author.id]['coolDown'] = (poopy.data['guild-data'][msg.guild.id]['members'][msg.author.id]['coolDown'] || Date.now()) + command.cooldown / ((msg.member.permissions.has('MANAGE_GUILD') || msg.member.roles.cache.find(role => role.name.match(/mod|dev|admin|owner|creator|founder|staff/ig)) || msg.member.permissions.has('MANAGE_MESSAGES') || msg.member.permissions.has('ADMINISTRATOR') || msg.author.id === msg.guild.ownerID) && (command.type === 'Text' || command.type === 'Main') ? 5 : 1)
+                    }
+
+                    poopy.vars.cps++
+                    poopy.data['bot-data']['commands']++
+                    var t = setTimeout(() => {
+                        poopy.vars.cps--;
+                        clearTimeout(t)
+                    }, 1000)
+
+                    poopy.functions.infoPost(`Command \`${commandname}\` used`)
+                    await command.execute.call(mainPoopy, msg, args).catch((e) => {
+                        if (!sent) res.type('text').send(e.stack)
+                    })
+                    poopy.data['bot-data']['filecount'] = poopy.vars.filecount
+                } else if (localCommand) {
+                    poopy.vars.cps++
+                    poopy.data['bot-data']['commands']++
+                    var t = setTimeout(() => {
+                        poopy.vars.cps--;
+                        clearTimeout(t)
+                    }, 60000)
+
+                    poopy.functions.infoPost(`Command \`${commandname}\` used`)
+                    var phrase = await poopy.functions.getKeywordsFor(localCommand.phrase, msg, true).catch(() => { }) ?? 'error'
+                    poopy.data['bot-data']['filecount'] = poopy.vars.filecount
+                    if (!sent) res.type('text').send(phrase)
+                }
+            } else {
+                res.status(400).type('text').send('Invalid command.')
             }
         } else {
             res.status(102).res.sendFile(`${__dirname}/https/startpage.html`)
@@ -279,6 +349,7 @@ async function main() {
         if (!mainPoopy) mainPoopy = poopy
     }
     poopyStarted = true
+    eventEmitter.emit('poopystarted')
 }
 
 main()
