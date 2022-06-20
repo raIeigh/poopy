ec9b85788721eb139a65e020cb310d841e156d7e
raleigh <rbnpt@hotmail.com>
2022-06-20 02:53:25 +0100

    made sort() support uppercase

--- special/functions/sort.js
+++ special/functions/sort.js
@@ -9,7 +9,11 @@ module.exports = {
     var array = poopy.tempdata[msg.author.id]['arrays'][word]
     if (!array) return ''
 
-    array.sort()
+    array.sort((a, b) => {
+        if (a.toLowerCase() < b.toLowerCase()) return -1
+        if (a.toLowerCase() > b.toLowerCase()) return 1
+        return 0
+    })
 
     return ''
   }
--- ver.js
+++
@@ -1,8 +0,0 @@
-const dir = 'cmds'
-const fs = require('fs')
-
-fs.readdirSync(dir).forEach(name => {
-    var file = fs.readFileSync(`${dir}/${name}`).toString()
-    
-    if (file.includes('role => role.name')) console.log(name)
-})
