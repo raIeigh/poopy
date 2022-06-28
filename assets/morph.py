c72f6cdcd65737c4bac3a2d0dc123a25ef09dc79
raleigh <rbnpt@hotmail.com>
2022-06-28 17:34:58 +0100

    m

--- assets/morph.py
+++ assets/morph.py
@@ -1,5 +1,4 @@
 import gmic
 import sys
 
-gmic.run(sys.argv[1] + '/static.png ' + sys.argv[1] +
-             '/static2.png morph 20 ' + sys.argv[1] + '/frames/frame.png')
+gmic.run(sys.argv[1] + '/static.png ' + sys.argv[1] + '/static2.png morph 20 output ' + sys.argv[1] + '/frames/frame.png')
