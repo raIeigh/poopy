81ddeb3d394199c9bee180c7e5e7f46937a7f3ed
raleigh <rbnpt@hotmail.com>
2022-07-23 02:20:20 +0100

    temperature setting

--- cmds/text/generatetext.js
+++ cmds/text/generatetext.js
@@ -5,8 +5,9 @@ module.exports = {
 
         await msg.channel.sendTyping().catch(() => { })
 
+        var temperature = poopy.functions.getOption(args, 'temperature', { dft: 0.6, splice: true, n: 1, join: true, func: (opt) => poopy.functions.parseNumber(opt, { dft: 0.6, min: 0, max: 1, round: false }) })
         var maxtokens = poopy.functions.getOption(args, 'maxtokens', { dft: 65, splice: true, n: 1, join: true, func: (opt) => poopy.functions.parseNumber(opt, { dft: 65, min: 1, max: 2048, round: true }) })
-        var pres = poopy.functions.getOption(args, 'prespenalty', { dft: 0, splice: true, n: 1, join: true, func: (opt) => poopy.functions.parseNumber(opt, { dft: 0, min: 0, max: 5, round: false }) })
+        var pres = poopy.functions.getOption(args, 'prespenalty', { dft: 1, splice: true, n: 1, join: true, func: (opt) => poopy.functions.parseNumber(opt, { dft: 1, min: 0, max: 5, round: false }) })
         var count = poopy.functions.getOption(args, 'countpenalty', { dft: 0, splice: true, n: 1, join: true, func: (opt) => poopy.functions.parseNumber(opt, { dft: 0, min: 0, max: 1, round: false }) })
         var freq = poopy.functions.getOption(args, 'freqpenalty', { dft: 0, splice: true, n: 1, join: true, func: (opt) => poopy.functions.parseNumber(opt, { dft: 0, min: 0, max: 500, round: true }) })
         var saidMessage = args.slice(1).join(' ')
@@ -23,7 +24,7 @@ module.exports = {
                 prompt: saidMessage,
                 numResults: 1,
                 maxTokens: maxtokens,
-                temperature: 0.6,
+                temperature: temperature,
                 topKReturn: 0,
                 topP: 1,
                 presencePenalty: {
@@ -85,8 +86,8 @@ module.exports = {
         await msg.channel.sendTyping().catch(() => { })
     },
     help: {
-        name: 'generatetext/predicttext <message> [-maxtokens <number (max 2048)>] [-(pres/count/freq)penalty <number (max 5/1/500)>]',
-        value: 'Tries to predict subsequent text from the specified message with AI21. Default max tokens are 65.'
+        name: 'generatetext/predicttext <message> [-temperature <number (from 0 to 1)] [-maxtokens <number (max 2048)>] [-(pres/count/freq)penalty <number (from 0 to 5/1/500)>]',
+        value: 'Tries to predict subsequent text from the specified message with AI21. Default max tokens are 65 and temperature is 0.6.'
     },
     type: 'Text',
     envRequired: ['AI21KEY']
