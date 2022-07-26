bd832e4f72411564b75262b76792e319ff70a410
raleigh <rbnpt@hotmail.com>
2022-07-26 22:38:52 +0100

    wait() tweaks

--- special/functions/wait.js
+++ special/functions/wait.js
@@ -1,14 +1,13 @@
 module.exports = {
   helpf: '(seconds)',
   desc: 'Waits the specified seconds. Maximum is 60.',
-  func: async function (matches) {
+  func: async function (matches, msg) {
     let poopy = this
 
     var word = matches[1]
-    var waitTime = Number(word)
-    await poopy.functions.sleep((waitTime * 1000) > 60000 ? 60000 : waitTime * 1000)
+    var waitTime = Math.min(Math.max(Number(word), 0), 60)
+    poopy.tempdata[msg.author.id][msg.id]['keyattempts'] += !isNaN(waitTime) ? waitTime : 0
+    await poopy.functions.sleep(waitTime * 1000)
     return ''
-  },
-  attemptvalue: 10,
-  limit: 5
+  }
 }
