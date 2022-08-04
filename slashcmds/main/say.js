dd6c3af751d5bdba82cf1eaf79361e32f082e53b
raleigh <rbnpt@hotmail.com>
2022-08-04 23:01:51 +0100

    The new say

--- slashcmds/main/say.js
+++ slashcmds/main/say.js
@@ -25,6 +25,12 @@ const commandObject = {
                 .setRequired(false)
         )
 
+        .addBooleanOption(option =>
+            option.setName('tts')
+                .setDescription('Whether to enable TTS in the message or not.')
+                .setRequired(false)
+        )
+
         .toJSON(),
 
     async execute(interaction) {
@@ -40,27 +46,46 @@ const commandObject = {
         const content = options.getString('content');
         const dmUser = options.getUser('user');
         const repeat = options.getInteger('repeat');
+        const tts = options.getBoolean('tts') ?? false;
 
         const channelToSend = dmUser ?? channel;
 
-        if (repeat && !(permissions.has('ADMINISTRATOR') || ermissions.has('MANAGE_MESSAGES') || user.id == guild.ownerId)) {
-            await interaction.reply('Fat').catch(() => { });
+        if (repeat && repeat > 1 && !(permissions.has('ADMINISTRATOR') || permissions.has('MANAGE_MESSAGES') || user.id == guild.ownerId)) {
+            await interaction.reply({
+                content: 'You need the manage messages permission to send more than 1 message!',
+                ephemeral: true
+            }).catch(() => { });
             return
         }
 
-        await interaction.deferReply().catch(() => { });
-        await interaction.deleteReply().catch(() => { });
+        if (tts && !(permissions.has('ADMINISTRATOR') || permissions.has('SEND_TTS_MESSAGES') || user.id == guild.ownerId)) {
+            await interaction.reply({
+                content: 'You need the send TTS messages permissions to send them!',
+                ephemeral: true
+            }).catch(() => { });
+            return
+        }
+
+        await interaction.deferReply({ ephemeral: true }).catch(() => { });
+
+        let error
 
         for (let i = 1; i <= (repeat ?? 1); i++)
             await channelToSend.send({
                 content: content,
+                tts: tts,
                 allowedMentions: {
                     parse: (permissions.has('ADMINISTRATOR') ||
                         permissions.has('MENTION_EVERYONE') ||
                         user.id == guild.ownerId) ?
                         ['users', 'everyone', 'roles'] : ['users']
                 }
-            }).catch(() => { });
+            }).catch((e) => error = e.message);
+
+        await interaction.editReply({
+            content: error ?? 'Successfully sent.',
+            ephemeral: true
+        }).catch(() => { });
     },
 
     cooldown: 2500,
