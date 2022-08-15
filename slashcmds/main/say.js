98aa6ca4a9305aa33f01181150e7abca67f3bdc5
raleigh <rbnpt@hotmail.com>
2022-08-15 17:56:08 +0100

    I hate you

--- slashcmds/main/say.js
+++ slashcmds/main/say.js
@@ -15,7 +15,7 @@ const commandObject = {
             option.setName('user')
                 .setDescription('An user to DM the content.')
                 .setRequired(false)
-        )*/
+        )
 
         .addIntegerOption(option =>
             option.setName('repeat')
@@ -23,7 +23,7 @@ const commandObject = {
                 .setMinValue(1)
                 .setMaxValue(25)
                 .setRequired(false)
-        )
+        )*/
 
         .addBooleanOption(option =>
             option.setName('tts')
@@ -44,11 +44,72 @@ const commandObject = {
         const permissions = member.permissions;
 
         const content = options.getString('content');
-        //const dmUser = options.getUser('user');
-        const repeat = options.getInteger('repeat');
+        const dmUser = null//options.getUser('user');
+        const repeat = null//options.getInteger('repeat');
         const tts = options.getBoolean('tts') ?? false;
 
-        const channelToSend = channel//dmUser ?? channel;
+        if (dmUser) {
+            if (!poopy.data['user-data'][dmUser.id]) {
+                poopy.data['user-data'][dmUser.id] = {}
+            }
+            if (!poopy.tempdata[dmUser.id]) {
+                poopy.tempdata[dmUser.id] = {}
+            }
+            if (dmUser.id === user.id) {
+                dmUser.send({
+                    content: (anon ? '' : `${user.tag} from ${msg.guild.name}:\n\n`) + saidMessage,
+                    files: attachments
+                }).then(async () => {
+                    msg.react('✅').catch(() => { })
+                    await msg.channel.sendTyping().catch(() => { })
+                }).catch(async () => {
+                    await msg.channel.send('unblock me').catch(() => { })
+                    await msg.channel.sendTyping().catch(() => { })
+                    return
+                })
+                return
+            }
+            if (poopy.data['user-data'][member.id]['dms'] === undefined && !poopy.tempdata[member.id]['dmconsent']) {
+                poopy.tempdata[msg.author.id]['dmconsent'] = true
+
+                var pending = await msg.channel.send('Pending response.').catch(() => { })
+                var send = await poopy.functions.yesno(member, `${!anon ? msg.author.tag : 'Someone'} is trying to send you a message. Will you consent to any unrelated DMs sent with the \`dm\` command?`, member.id).catch(() => { })
+
+                if (send !== undefined) {
+                    poopy.data['user-data'][member.id]['dms'] = send
+                    member.send({
+                        content: `Unrelated DMs from \`dm\` will **${!send ? 'not ' : ''}be sent** to you now.`,
+                        allowedMentions: {
+                            parse: ((!msg.member.permissions.has('ADMINISTRATOR') && !msg.member.permissions.has('MENTION_EVERYONE') && msg.author.id !== msg.guild.ownerID) && ['users']) || ['users', 'everyone', 'roles']
+                        }
+                    }).catch(() => { })
+                    if (pending) {
+                        pending.edit(send ? 'You can send DMs to the user now.' : 'blocked on twitter').catch(() => { })
+                    }
+                } else {
+                    pending.edit('Couldn\'t send a message to this user. Make sure they share any of the servers I\'m in, or not have me blocked.').catch(() => { })
+                }
+            } else {
+                if (poopy.data['user-data'][member.id]['dms'] === false) {
+                    await msg.channel.send('I don\'t have the permission to send unrelated DMs to this user.').catch(() => { })
+                    await msg.channel.sendTyping().catch(() => { })
+                    return
+                }
+                member.send({
+                    content: (!anon ? `${msg.author.tag} from ${msg.guild.name}:\n\n` : '') + saidMessage,
+                    files: attachments
+                }).then(async () => {
+                    msg.react('✅').catch(() => { })
+                    await msg.channel.sendTyping().catch(() => { })
+                }).catch(async () => {
+                    await msg.channel.send('Couldn\'t send a message to this user. Make sure they share any of the servers I\'m in, or not have me blocked.').catch(() => { })
+                    await msg.channel.sendTyping().catch(() => { })
+                    return
+                })
+            }
+        }
+
+        const channelToSend = dmUser ?? channel;
 
         if (repeat && repeat > 1 && !(permissions.has('ADMINISTRATOR') || permissions.has('MANAGE_MESSAGES') || user.id == guild.ownerId)) {
             await interaction.reply({
