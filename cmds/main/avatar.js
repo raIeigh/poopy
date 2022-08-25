623c6d25ae76b468bc288357a1ca523666281e8d
raleigh <rbnpt@hotmail.com>
2022-08-25 15:31:35 +0100

    ser

--- cmds/main/avatar.js
+++ cmds/main/avatar.js
@@ -1,16 +1,35 @@
 module.exports = {
-    name: ['avatar', 'av', 'pfp'],
-    args: [{"name":"user","required":false,"specifarg":false,"orig":"[user]"}],
+    name: ['avatar',
+        'av',
+        'pfp'],
+    args: [{
+        "name": "user",
+        "required": false,
+        "specifarg": false,
+        "orig": "[user]",
+        "autocomplete": function (interaction) {
+            let poopy = this
+
+            var memberData = poopy.data['guild-data'][interaction.guild.id]['members']
+            var memberKeys = Object.keys(memberData).sort((a, b) => memberData[b].messages - memberData[a].messages)
+
+            return memberKeys.map(id => {
+                return {
+                    name: memberData[id].username, value: id
+                }
+            })
+        }
+    }],
     execute: async function (msg, args) {
         let poopy = this
 
-        await msg.channel.sendTyping().catch(() => { })
+        await msg.channel.sendTyping().catch(() => {})
 
         args[1] = args[1] ?? ''
 
         var member = (msg.mentions.members.first() && msg.mentions.members.first().user) ??
-            await poopy.bot.users.fetch((args[1].match(/\d+/) ?? [args[1]])[0]).catch(() => { }) ??
-            msg.author
+        await poopy.bot.users.fetch((args[1].match(/\d+/) ?? [args[1]])[0]).catch(() => {}) ??
+        msg.author
 
         if (!member) {
             await msg.reply({
@@ -18,18 +37,20 @@ module.exports = {
                 allowedMentions: {
                     parse: ((!msg.member.permissions.has('ADMINISTRATOR') && !msg.member.permissions.has('MENTION_EVERYONE') && msg.author.id !== msg.guild.ownerID) && ['users']) || ['users', 'everyone', 'roles']
                 }
-            }).catch(() => { })
+            }).catch(() => {})
             return
         }
 
-        var avatar = new poopy.modules.Discord.MessageAttachment(member.displayAvatarURL({ dynamic: true, size: 1024, format: 'png' }));
+        var avatar = new poopy.modules.Discord.MessageAttachment(member.displayAvatarURL({
+            dynamic: true, size: 1024, format: 'png'
+        }));
         await msg.reply({
             content: member.username + '\'s avatar is:',
             allowedMentions: {
                 parse: ((!msg.member.permissions.has('ADMINISTRATOR') && !msg.member.permissions.has('MENTION_EVERYONE') && msg.author.id !== msg.guild.ownerID) && ['users']) || ['users', 'everyone', 'roles']
             },
             files: [avatar]
-        }).catch(() => { })
+        }).catch(() => {})
     },
     help: {
         name: 'avatar/av/pfp [user]',
