c3d6605d129e9996ac5ab299eeb0d2ab5ba8c7f7
raleigh <rbnpt@hotmail.com>
2022-09-07 13:01:27 +0100

    i keep missing things

--- src/commands/memes/fail.js
+++ src/commands/memes/fail.js
@@ -27,21 +27,21 @@ module.exports = {
                 fileinfo: fileinfo
             })
             var filename = `input.png`
-            await execPromise(`ffmpeg -i ${filepath}/${filename} -i assets/norris.png -filter_complex "[0:v]scale=508:257[frame];[1:v][frame]overlay=x=7:y=306:format=auto[out]" -map "[out]" -preset ${findpreset(args)} ${filepath}/output.png`)
+            await execPromise(`ffmpeg -i ${filepath}/${filename} -i assets/image/norris.png -filter_complex "[0:v]scale=508:257[frame];[1:v][frame]overlay=x=7:y=306:format=auto[out]" -map "[out]" -preset ${findpreset(args)} ${filepath}/output.png`)
             return await sendFile(msg, filepath, `output.png`)
         } else if (type.mime.startsWith('video')) {
             var filepath = await downloadFile(currenturl, `input.mp4`, {
                 fileinfo: fileinfo
             })
             var filename = `input.mp4`
-            await execPromise(`ffmpeg -i ${filepath}/${filename} -i assets/norris.png -map 0:a? -filter_complex "[0:v]scale=508:257[frame];[1:v][frame]overlay=x=7:y=306:format=auto[oout];[oout]scale=ceil(iw/2)*2:ceil(ih/2)*2[out]" -map "[out]" -preset ${findpreset(args)} -c:v libx264 -pix_fmt yuv420p ${filepath}/output.mp4`)
+            await execPromise(`ffmpeg -i ${filepath}/${filename} -i assets/image/norris.png -map 0:a? -filter_complex "[0:v]scale=508:257[frame];[1:v][frame]overlay=x=7:y=306:format=auto[oout];[oout]scale=ceil(iw/2)*2:ceil(ih/2)*2[out]" -map "[out]" -preset ${findpreset(args)} -c:v libx264 -pix_fmt yuv420p ${filepath}/output.mp4`)
             return await sendFile(msg, filepath, `output.mp4`)
         } else if (type.mime.startsWith('image') && vars.gifFormats.find(f => f === type.ext)) {
             var filepath = await downloadFile(currenturl, `input.gif`, {
                 fileinfo: fileinfo
             })
             var filename = `input.gif`
-            await execPromise(`ffmpeg -i ${filepath}/${filename} -i assets/norris.png -filter_complex "[0:v]scale=508:257[frame];[1:v][frame]overlay=x=7:y=306:format=auto[oout];[oout]split[pout][ppout];[ppout]palettegen=reserve_transparent=1[palette];[pout][palette]paletteuse=alpha_threshold=128[out]" -map "[out]" -preset ${findpreset(args)} -gifflags -offsetting ${filepath}/output.gif`)
+            await execPromise(`ffmpeg -i ${filepath}/${filename} -i assets/image/norris.png -filter_complex "[0:v]scale=508:257[frame];[1:v][frame]overlay=x=7:y=306:format=auto[oout];[oout]split[pout][ppout];[ppout]palettegen=reserve_transparent=1[palette];[pout][palette]paletteuse=alpha_threshold=128[out]" -map "[out]" -preset ${findpreset(args)} -gifflags -offsetting ${filepath}/output.gif`)
             return await sendFile(msg, filepath, `output.gif`)
         } else {
             await msg.reply({
