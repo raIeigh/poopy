df131d356e87d3fa0f8ebbc8e43a79eb38a396ec
raleigh <rbnpt@hotmail.com>
2022-10-26 14:51:00 +0100

    _eight keyword

---
+++ src/special/keys/_eight.js
@@ -0,0 +1,8 @@
+module.exports = {
+    desc: 'Returns a random 8-Ball phrase.', func: function () {
+        let poopy = this
+        let arrays = poopy.arrays
+
+        return arrays.eightball[Math.floor(Math.random() * arrays.eightball.length)]
+    }
+}
