2ece3bbce859de13ee770a91ce18fa16e50b8549
raIeigh <58990470+raIeigh@users.noreply.github.com>
2022-12-04 23:33:17 +0000

    _file

---
+++ src/special/keys/_file.js
@@ -0,0 +1,45 @@
+module.exports = {
+  desc: 'Returns a random file sent in the server. Requires permission for the bot to read messages within a channel.',
+  func: function (msg) {
+    let poopy = this
+    let data = poopy.data
+    let vars = poopy.vars
+    let { decrypt } = poopy.functions
+    
+    var validImage = new RegExp(`${vars.validUrl.source}\\.(png|jpe?g|bmp|webp|gif|apng|mp4|mov|wmv|webp|avi)`)
+
+    var messages = data.guildData[msg.guild.id]['messages'].filter(m => decrypt(m.content).match(validImage))
+    var urlMessages = []
+    
+    for (var { ...m } of messages) {
+        var urls = decrypt(m.content).match(new RegExp(validImage, 'g'))
+        for (var url of urls) {
+            m.content = url
+            urlMessages.push({ ...m })
+        }
+    }
+    
+    return urlMessages.length ? urlMessages[Math.floor(Math.random() * urlMessages.length)].content.replace(/\@/g, '@‌') : ''
+  },
+  array: function (msg) {
+    let poopy = this
+    let data = poopy.data
+    let vars = poopy.vars
+    let { decrypt } = poopy.functions
+    
+    var validImage = new RegExp(`${vars.validUrl.source}\\.(png|jpe?g|bmp|webp|gif|apng|mp4|mov|wmv|webp|avi)`)
+
+    var messages = data.guildData[msg.guild.id]['messages'].filter(m => decrypt(m.content).match(validImage))
+    var urlMessages = []
+    
+    for (var { ...m } of messages) {
+        var urls = decrypt(m.content).match(new RegExp(validImage, 'g'))
+        for (var url of urls) {
+            m.content = url
+            urlMessages.push({ ...m })
+        }
+    }
+
+    return urlMessages.map(m => m.content.replace(/\@/g, '@‌'))
+  }
+}
