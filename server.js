f73cdead82342330389de8629bbe71f39f19ef78
raIeigh <58990470+raIeigh@users.noreply.github.com>
2022-12-08 23:29:24 +0000

    automatically set the url

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ async function start() {
     let poopy
     let { APIMessage } = require('./src/modules')
     let { sleep, escapeHTML } = require('./src/functions')
+    
+    if (process.env.RAILWAY_STATIC_URL && !process.env.BOT_WEBSITE) process.env.BOT_WEBSITE = `https://${process.env.RAILWAY_STATIC_URL}`
 
     if (process.env.BOT_WEBSITE) {
         const express = require('express')
