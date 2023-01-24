868dd59e5dd3d9fc99d4285b95079d4f818a582f
wing dev <109431644+OrdinarySoftwareDev@users.noreply.github.com>
2023-01-24 19:56:19 +0100

    paws aren't segsy you dummies

--- src/special/keys/_segsy.js
+++ src/special/keys/_segsy.js
@@ -2,8 +2,8 @@ module.exports = {
     desc: 'this is the worst keyword', func: function () {
         let poopy = this
 
-        var segsy = ['boobs', 'breasts', 'cleavage', 'bosoms', 'pussy', 'vagina', 'ass', 'asshole', 'butt', 'buttocks', 'bum', 'bussy', 'boy pussy', 'penis', 'cock', 'dick', 'balls', 'testicles', 'scrotum', 'perineum', 'thighs', 'legs', 'mouth', 'feet', 'cum', 'paws', 'toes', 'anus', 'pawpads', 'foreskin', 'pubes', 'tits', 'titties', 'areolas', 'milkers']
+        var segsy = ['boobs', 'breasts', 'cleavage', 'bosoms', 'pussy', 'vagina', 'ass', 'asshole', 'butt', 'buttocks', 'bum', 'bussy', 'boy pussy', 'penis', 'cock', 'dick', 'balls', 'testicles', 'scrotum', 'perineum', 'thighs', 'legs', 'mouth', 'feet', 'cum', 'toes', 'anus', 'foreskin', 'pubes', 'tits', 'titties', 'areolas', 'milkers', 'asshole']
         return segsy[Math.floor(Math.random() * segsy.length)]
     },
-    array: ['boobs', 'breasts', 'cleavage', 'bosoms', 'pussy', 'vagina', 'ass', 'asshole', 'butt', 'buttocks', 'bum', 'bussy', 'boy pussy', 'penis', 'cock', 'dick', 'balls', 'testicles', 'scrotum', 'perineum', 'thighs', 'legs', 'mouth', 'feet', 'cum', 'paws', 'toes', 'anus', 'pawpads', 'foreskin', 'pubes', 'tits', 'titties', 'areolas', 'milkers']
+    array: ['boobs', 'breasts', 'cleavage', 'bosoms', 'pussy', 'vagina', 'ass', 'asshole', 'butt', 'buttocks', 'bum', 'bussy', 'boy pussy', 'penis', 'cock', 'dick', 'balls', 'testicles', 'scrotum', 'perineum', 'thighs', 'legs', 'mouth', 'feet', 'cum', 'toes', 'anus', 'foreskin', 'pubes', 'tits', 'titties', 'areolas', 'milkers', 'asshole']
 }
