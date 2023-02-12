8c992a01a28b3af3043f9a3fc5d2c7e875244a0a
BilonGamer <81695336+BilonGamer@users.noreply.github.com>
2023-02-13 00:15:44 +0400

    get headers

--- src/special/functions/get.js
+++ src/special/functions/get.js
@@ -1,15 +1,16 @@
 module.exports = {
-  helpf: '(url)',
+  helpf: '(url | headers)',
   desc: "GETs the content from the URL, but only if it isn't binary. Useful if your command hits the 2000 character limit or wanna use a free API.",
   func: async function (matches) {
     let poopy = this
+    let { splitKeyFunc } = poopy.functions
     let { axios, itob } = poopy.modules
 
-    var word = matches[1]
+    var [ url, headers ] = splitKeyFunc(matches[1], { args: 2 })
 
-    if (word.includes("ip")) return "no doxxing for you lmao";
+    if (url.includes("ip")) return "no doxxing for you lmao";
 
-    var res = await axios.get(word, { responseType: 'arraybuffer' }).catch(() => { })
+    var res = await axios.get(url, { headers: JSON.parse(headers), responseType: 'arraybuffer' }).catch(() => { })
 
     if (!res || itob.isBinary(null, res.data)) return word
 
