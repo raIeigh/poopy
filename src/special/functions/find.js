ac04ec1a1772ee97c09712df98c40d3216e3f68e
raleigh <58990470+raIeigh@users.noreply.github.com>
2023-03-20 08:52:27 +0000

    optimization1

--- src/special/functions/find.js
+++ src/special/functions/find.js
@@ -3,7 +3,7 @@ module.exports = {
     desc: 'Finds a value in the array that matches the function.',
     func: async function (matches, msg, isBot, _, opts) {
         let poopy = this
-        let { splitKeyFunc, getKeywordsFor, findAsync } = poopy.functions
+        let { splitKeyFunc, getKeywordsFor, chunkArray, findAsync } = poopy.functions
         let tempdata = poopy.tempdata
 
         var word = matches[1]
@@ -14,18 +14,25 @@ module.exports = {
         var array = tempdata[msg.author.id]['arrays'][name]
         if (!array) return ''
 
-        return await findAsync(array, async (val) => {
-            var valOpts = { ...opts }
-            valOpts.extrakeys._val = {
-                func: async () => {
-                    return val
+        var chunks = chunkArray(array, 50)
+        for (var chunk of chunks) {
+            var find = await findAsync(chunk, async (val) => {
+                var valOpts = { ...opts }
+                valOpts.extrakeys._val = {
+                    func: async () => {
+                       return val
+                    }
                 }
-            }
 
-            var found = await getKeywordsFor(func, msg, isBot, valOpts).catch(() => { }) ?? ''
+                var found = await getKeywordsFor(func, msg, isBot, valOpts).catch(() => { }) ?? ''
 
-            return found
-        }).catch(() => { }) ?? ''
+                return found
+            }).catch(() => { })
+
+            if (find) return find
+        }
+
+        return ''
     },
     attemptvalue: 5,
     raw: true
