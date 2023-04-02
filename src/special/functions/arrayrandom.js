b454b369e6843a479ec470b8bccb109beefe4314
BilonGamer <81695336+BilonGamer@users.noreply.github.com>
2023-04-03 00:22:20 +0400

    arrayrandom, finally.

--- src/special/functions/arrayrandom.js
+++ src/special/functions/arrayrandom.js
@@ -1,12 +1,12 @@
 module.exports = {
   helpf: '(arrayName)',
-  desc: 'Returns a random element from the array',
+  desc: 'Returns a random element from the array.',
   func: function (matches, msg) {
     let poopy = this
     let tempdata = poopy.tempdata
 
     var word = matches[1]
     var array = tempdata[msg.author.id]['arrays'][word] ?? []
-    return array[Math.floor(Math.random()*array.length)]
+    return array.length > 0 : array[Math.floor(Math.random()*array.length)] ? ''
   }
 }
