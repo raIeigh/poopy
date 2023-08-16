457d1d57bbd609a00d6028098e9a1bfb81f2584a
raleigh <58990470+raIeigh@users.noreply.github.com>
2023-08-16 15:06:05 +0100

    the new

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
 const throng = require('throng')
+const fs = require('fs-extra')
+
+function updateEnvironment() {
+    if (!fs.existsSync('.env')) return
+
+    const env = fs.readFileSync('.env').toString()
+
+    for (const line of env.split(' ')) {
+        if (!line || !line.includes("=") || line.startsWith("#")) continue
+
+        let [key, value] = line.split("=", 2)
+        if (!key || !value || process.env[key]) continue
+
+        process.env[key] = value
+    }
+}
 
 async function start() {
     let poopyStarted = false
@@ -220,4 +236,5 @@ async function start() {
     }
 }
 
+updateEnvironment()
 throng({ workers: 1, start }) // My poopy will never die
