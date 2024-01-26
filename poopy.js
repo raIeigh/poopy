a34975e2290d66d0dcdb8a42afcaa12e13e7af9d
BilonGamer <81695336+BilonGamer@users.noreply.github.com>
2024-01-26 22:23:59 +0400

    hivemind part 1

--- poopy.js
+++ poopy.js
@@ -521,6 +521,7 @@ class Poopy {
             if (dataError) return console.log(dataError)
 
             var prefix = data.guildData[msg.guild.id]?.['prefix'] ?? config.globalPrefix
+            var hivemind = data.guildData[msg.guild.id]['poopymode'] && "On" ?? "Off"
 
             if (msg.channel.type == Discord.ChannelType.DM && msg.type !== Discord.InteractionType.ApplicationCommand && !msg.content.includes(prefix)) {
                 if (msg.author.bot || msg.author.id == bot.user.id) return
@@ -1024,21 +1025,21 @@ class Poopy {
 
             if (config.allowpingresponses && msg.mentions.members.find(member => member.user.id === bot.user.id) && ((!msg.author.bot && msg.author.id != bot.user.id) || config.allowbotusage) && !executed) {
                 var eggPhrases = [
-                    `My prefix here is \`${prefix}\``,
-                    `My prefix here is \`${prefix}\``,
-                    `My prefix here is \`${prefix}\``,
-                    `Did you know my prefix here is \`${prefix}\`?`,
-                    `Is my prefix \`${prefix}\`?`,
-                    `So, \`${prefix}\``,
-                    `\`${prefix}\``,
-                    `it's \`${prefix}\``,
+                    `My prefix here is \`${prefix}\`\nHivemind mode is **${hivemind}**`,
+                    `My prefix here is \`${prefix}\`\nHivemind mode is **${hivemind}**`,
+                    `My prefix here is \`${prefix}\`\nHivemind mode is **${hivemind}**`,
+                    `Did you know my prefix here is \`${prefix}\` and hivemind mode is **${hivemind}**?`,
+                    `Is my prefix \`${prefix}\`? Is hivemind mode really **${hivemind}**?`,
+                    `So, \`${prefix}\` and hivemind **${hivemind}**`,
+                    `\`${prefix}\`, **${hivemind}**`,
+                    `it's \`${prefix}\` and hivemind **${hivemind}**`,
                     `IT'S \`${prefix}\`!!!!!!!!`,
                     `\`${prefix}\`\`${prefix}\`\`${prefix}\`\`${prefix}\`\`${prefix}\`\`${prefix}\`\`${prefix}\`\`${prefix}\`\`${prefix}\`\`${prefix}\`\`${prefix}\`\`${prefix}\``,
                     'are you serious',
                     'a',
                     'please stop',
                     'lmao!!',
-                    `its \`${prefix}\` thats it THAT'S FUCKING IT`,
+                    `its \`${prefix}\` with hivemind being **${hivemind}** thats it THAT'S FUCKING IT`,
                     `it's that easy`,
                     `do you`,
                     `do you know how to use commands`,
@@ -1073,7 +1074,7 @@ class Poopy {
                     'please',
                     'xd.',
                     'okay i gave up on you!',
-                    'gotta wait 1 minute if you want my prefix Lol!!!',
+                    'gotta wait 1 minute if you want my prefix and hivemind mode status Lol!!!',
                     ''
                 ]
 
---
+++ src/commands/main/alerthost.js
@@ -0,0 +1,42 @@
+let globalId = 1
+
+module.exports = {
+    name: ['alerthost'],
+    args: [{
+        "name": "text",
+        "required": true,
+        "specifarg": false,
+        "orig": "<text>",
+    }],
+    execute: async function (msg, args) {
+        let poopy = this
+        let { execPromise } = poopy.functions
+        let { fs } = poopy.modules
+
+        if (args[1] === undefined) {
+            if (!msg.nosend) msg.reply('What is the text to show?!')
+            return ''
+        }
+        
+        var saidMessage = args.slice(1).join(' ')
+        saidMessage = saidMessage.match(/[^"]+/g)[0].replace(/\n/g, '\\n')
+
+        var thisFileName = 'temp' + globalId + '.vbs'
+        globalId++
+
+        fs.writeFileSync(`./temp/${thisFileName}`, `x=MsgBox("${saidMessage}", 4144, "Bot Message")`)
+        execPromise(`wscript ./temp/${thisFileName}`).then(() => {
+            fs.unlinkSync(`./temp/${thisFileName}`)
+        })
+
+        if (!msg.nosend) msg.reply('Success.')
+
+        return ''
+    },
+    help: {
+        name: '<:newpoopy:839191885310066729> alerthost <text>',
+        value: "Alert's the bot's host machine with your message."
+    },
+    cooldown: 30000,
+    type: 'Main'
+}
