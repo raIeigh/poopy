78197b73dd36d2751616cec6a96ae2c4695d4e45
BilonGamer <81695336+BilonGamer@users.noreply.github.com>
2024-07-26 02:10:49 +0400

    angry luigi history.

--- src/commands/angryluigi/alchat.js
+++ src/commands/angryluigi/alchat.js
@@ -5,6 +5,7 @@ module.exports = {
     },],
     execute: async function (msg, args) {
         let poopy = this
+        let { tempdata } = poopy
         let { getOption, parseNumber, userToken } = poopy.functions
         let { axios, fs, Discord } = poopy.modules
         let vars = poopy.vars
@@ -12,28 +13,38 @@ module.exports = {
 
         await msg.channel.sendTyping().catch(() => { })
 
+        var instruct = 'You are Angry Luigi. Angry Luigi is a version of Luigi who has gone angry due to a depressing, scary, and a cold past. His anger shows in his responses and he uses curse words very frequently. In his own words, \"My name\'s Angry Luigi, and I\'m super fucking mad. I don\'t know what to do, I\'ll just sit alone and sad. There is nothing in this world that can make me smile. Honestly, it\'s complicated why I\'m so hostile. Think about my lonely past, depressing, scary, and cold. You can play games all you want but I\'ll no longer be controlled. My name\'s Angry Luigi, and I want to see you burn. So stay right there and let me kill you, it\'s just what I\'ve earned!\" Additionally, if there isn\'t obvious context in the user\'s message, make up a scenario for the context and don\'t break character as Angry Luigi.'
         var saidMessage = args.slice(1).join(' ')
         if (args[1] === undefined) {
-            await msg.reply('Stop fucking around.').catch(() => { })
+            await msg.reply('What is the text to generate?!').catch(() => { })
             await msg.channel.sendTyping().catch(() => { })
             return
         }
+
+        if (!tempdata[msg.channel.id]) tempdata[msg.channel.id] = {}
         
+        var history = tempdata[msg.channel.id]?.['chathistories']
+        if (!history) history = tempdata[msg.channel.id]['chathistories'] = {} 
+
+        var ourHistory = history[instruct]
+        if (!ourHistory) ourHistory = history[instruct] = [
+            {
+                role: "system",
+                content: instruct,
+            }
+        ]
+
+        ourHistory.push({
+            role: "user",
+            content: saidMessage
+        })
+
         var resp = await axios({
             url: `https://api.ai21.com/studio/v1/chat/completions`,
             method: 'POST',
             data: {
                 model: "jamba-instruct",
-                messages: [
-                    {
-                        role: "system",
-                        content: 'You are Angry Luigi. Angry Luigi is a version of Luigi who has gone angry due to a depressing, scary, and a cold past. His anger shows in his responses and he uses curse words very frequently. In his own words, \"My name\'s Angry Luigi, and I\'m super fucking mad. I don\'t know what to do, I\'ll just sit alone and sad. There is nothing in this world that can make me smile. Honestly, it\'s complicated why I\'m so hostile. Think about my lonely past, depressing, scary, and cold. You can play games all you want but I\'ll no longer be controlled. My name\'s Angry Luigi, and I want to see you burn. So stay right there and let me kill you, it\'s just what I\'ve earned!\" Additionally, if there isn\'t obvious context in the user\'s message, make up a scenario for the context and don\'t break character as Angry Luigi.',
-                    },
-                    {
-                        role: "user",
-                        content: saidMessage,
-                    },
-                ],
+                messages: ourHistory,
                 temperature: 1,
                 top_p: 1,
             },
@@ -43,6 +54,15 @@ module.exports = {
         }).catch(() => { })
 
         if (resp) {
+            ourHistory.push({
+                role: "assistant",
+                content: resp.data.choices[0].message.content
+            })
+
+            var tokenAmount = resp.data.usage.total_tokens
+
+            if (tokenAmount > 200000) ourHistory.slice(1, 1) // ._.
+
             if (!msg.nosend) await msg.reply({
                 content: resp.data.choices[0].message.content,
                 allowedMentions: {
